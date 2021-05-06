55117ddae8209f98958ef4e02b800789a5abebb5
cfff226 <cazziednb@outlook.com>
2021-05-06 09:47:55 +0000

    Add if statement to run code if module is being run directly
    - Get default IP
    - Get default PORT and convert to number
    - Set debug to True while app is being edited

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "hello world"
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0,0,0,0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
