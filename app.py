0e2c2d1f4a996f39e2d82abc47dc8b5ab9db339b
cfff226 <cazziednb@outlook.com>
2021-06-01 09:37:20 +0000

    Add else statement to include a flash message to state either username or password is incorrect
    - Return and redirect back to login page

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
-    import env 
+    import env
 
 
 app = Flask(__name__)
@@ -16,6 +16,7 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
+
 
 @app.route("/")
 @app.route("/home")
@@ -39,29 +40,36 @@ if existing_user:
     # Make sure hashed password matches user input
     if check_password_hash(
         existing_user["password"], request.form.get("password")):
-            session["user"] = request.form.get("username").lower()
+            session["user"]=request.form.get("username").lower()
             flash("Welcome", {}".format(request.form.get("username")))
+        else:
+            # Invalid password match
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+   
+
     return render_template("login.html", page_title="Log In")
 
-@app.route("/register", methods=["GET", "POST"])
+
+@ app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         # Check if username exists in database
-        existing_user = mongo.db.users.find_one(
+        existing_user=mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
 
-        register = {
+        register={
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
         }
         mongo.db.users.insert_one(register)
 
         # put the new user into 'session' cookie
-        session["user"] = request.form.get("username").lower()
+        session["user"]=request.form.get("username").lower()
     return render_template("register.html")
 
 
