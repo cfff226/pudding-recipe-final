efa59929f0d4ba0a8c85bd5d4a2a5f4ac480b76b
cfff226 <cazziednb@outlook.com>
2021-06-01 09:16:20 +0000

    Supply new user cookie with the username from the form input
    - Make username lowercase for consistency
    - Add flash message to contain a placeholder of Welcome then get the username from the form input so the users name shows

--- app.py
+++ app.py
@@ -39,6 +39,8 @@ if existing_user:
     # Make sure hashed password matches user input
     if check_password_hash(
         existing_user["password"], request.form.get("password")):
+            session["user"] = request.form.get("username").lower()
+            flash("Welcome", {}".format(request.form.get("username")))
     return render_template("login.html", page_title="Log In")
 
 @app.route("/register", methods=["GET", "POST"])
