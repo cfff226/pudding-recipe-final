8510a71cb5de1c8ca553a02b4272b740fd6d9892
cfff226 <cazziednb@outlook.com>
2021-06-02 11:40:27 +0000

    Add flash message to show registration is successful
    - Redirect the user to profile page upon successful registration, set username to be equal to same session cookie as user

--- app.py
+++ app.py
@@ -34,14 +34,14 @@ def login():
     if request.method == "POST":
         # Check if username exists in database
         existing_user = mongo.db.users.find_one(
-             {"username": request.form.get("username").lower()})
+            {"username": request.form.get("username").lower()})
 
         if existing_user:
             # Make sure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
             else:
                 # Invalid password match
                 flash("Incorrect Username and/or Password")
@@ -51,35 +51,39 @@ def login():
             # Username does not exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login", _external=True, _scheme='https'))
-   
+
     return render_template("login.html", page_title="Log In")
 
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
+    # Grab the session user's Username from database
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-        return render_template("profile.html", username=username)
+    return render_template("profile.html", username=username)
+
 
 @ app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         # Check if username exists in database
-        existing_user=mongo.db.users.find_one(
+        existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
 
-        register={
+        register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
         }
         mongo.db.users.insert_one(register)
 
         # put the new user into 'session' cookie
-        session["user"]=request.form.get("username").lower()
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
