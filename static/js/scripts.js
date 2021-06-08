26c9cb641be735bd50aaba243e0afdd5339c96b8
cfff226 <cazziednb@outlook.com>
2021-06-08 22:27:57 +0000

    Add code taken from external source and alter for these purposes
    - Add functions to add new fields and remove fields from ingredients and instructions form fields

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,45 @@
-/*!
-* Start Bootstrap - Blog Home v4.3.0 (https://startbootstrap.com/template/blog-home)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-home/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Code structure used and changed to suit the purpose of this website https://www.sanwebe.com/2013/03/addremove-input-fields-dynamically-with-jquery 
+
+// Load function once document is loaded
+$(document).ready(function () {
+// Declaration of variables used
+    var ingred_max_fields = 15; //maximum input boxes allowed
+    var instruc_max_fields = 15; //maximum input boxes allowed
+    var ingred_wrapper = $(".input_ingred_wrap"); //Fields wrapper
+    var instruc_wrapper = $(".input_instruc_wrap"); //Fields wrapper
+    var add_ingredient = $(".add_ingredient_button"); //Add button ID
+    var add_instruction = $(".add_instruction_button"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_ingredient).click(function (e) { //on add input button click
+        e.preventDefault();
+        if (x < ingred_max_fields) { //max input box allowed
+            x++; //text box increment
+            $(ingred_wrapper).append('<div><input type="text" input id="dessert_ingredients" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+        }
+    });
+
+// Remove form field upon click
+    $(ingred_wrapper).on("click", ".remove_field", function (e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+
+    var x = 1; //initlal text box count
+    $(add_instruction).click(function (e) { //on add input button click
+        e.preventDefault();
+        if (x < instruc_max_fields) { //max input box allowed
+            x++; //text box increment
+            $(instruc_wrapper).append('<div><input type="text" input id="dessert_instructions" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+        }
+    });
+
+// Remove form field upon click
+    $(instruc_wrapper).on("click", ".remove_field", function (e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+
+});
