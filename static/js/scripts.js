07ca0b39c25457b6ba6c0f94942657ce4e710212
cfff226 <cazziednb@outlook.com>
2021-06-22 09:42:53 +0000

    Update with new dependencies

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 // Load function once document is loaded
 $(document).ready(function () {
-// Declaration of variables used
+    // Declaration of variables used
     var ingred_max_fields = 15; //maximum input boxes allowed
     var instruc_max_fields = 15; //maximum input boxes allowed
     var ingred_wrapper = $(".input_ingred_wrap"); //Fields wrapper
@@ -10,32 +10,37 @@ $(document).ready(function () {
     var add_ingredient = $(".add_ingredient_button"); //Add button ID
     var add_instruction = $(".add_instruction_button"); //Add button ID
 
-    var x = 1; //initlal text box count
+    var ingred = 1; //initial text box count
+    var max_ingred = 20; //max text box count
+
     $(add_ingredient).click(function (e) { //on add input button click
         e.preventDefault();
-        if (x < ingred_max_fields) { //max input box allowed
-            x++; //text box increment
-            $(ingred_wrapper).append('<div><input type="text" input id="dessert_ingredients" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+        if (ingred < ingred_max_fields) { //max input box allowed
+            ingred++; //text box increment
+            $(ingred_wrapper).append('<div><input type="text" input id="dessert_ingredients" name="dessert_ingredients"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
         }
     });
 
-// Remove form field upon click
+
+    // Remove form field upon click
     $(ingred_wrapper).on("click", ".remove_field", function (e) { //user click on remove text
         e.preventDefault();
         $(this).parent('div').remove();
         x--;
     })
 
-    var x = 1; //initlal text box count
+    let instruc = 1; // Initial text box count 
+    let max_instruc = 20; // Maximum text box count 
+
     $(add_instruction).click(function (e) { //on add input button click
         e.preventDefault();
-        if (x < instruc_max_fields) { //max input box allowed
-            x++; //text box increment
-            $(instruc_wrapper).append('<div><input type="text" input id="dessert_instructions" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+        if (instruc < instruc_max_fields) { //max input box allowed
+            instruc++; //text box increment
+            $(instruc_wrapper).append('<div><input type="text" input id="dessert_instructions" name="dessert_instructions"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
         }
     });
 
-// Remove form field upon click
+    // Remove form field upon click
     $(instruc_wrapper).on("click", ".remove_field", function (e) { //user click on remove text
         e.preventDefault();
         $(this).parent('div').remove();
